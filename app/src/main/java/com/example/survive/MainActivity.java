a8d5219da2b129dcab738c5b60e8ffb538ec4408
naynikaw <nay9139@gmail.com>
2019-05-05 12:09:57 +0530

    added map activity

--- app/src/main/java/com/example/survive/MainActivity.java
+++ app/src/main/java/com/example/survive/MainActivity.java
@@ -4,9 +4,14 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import com.ebanx.swipebtn.OnStateChangeListener;
 import com.ebanx.swipebtn.SwipeButton;
@@ -16,10 +21,22 @@ import com.google.android.gms.tasks.OnSuccessListener;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout dl;
+    private ActionBarDrawerToggle abdt;
+
+
     SwipeButton swipe_me;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        dl = (DrawerLayout)findViewById(R.id.dl);
+        abdt = new ActionBarDrawerToggle(this,dl,R.string.Open,R.string.Close);
+        abdt.setDrawerIndicatorEnabled(true);
+
+        dl.addDrawerListener(abdt);
+        abdt.syncState();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -31,6 +48,26 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        NavigationView nav_view = (NavigationView)findViewById(R.id.nav_view);
+        nav_view.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                int id = menuItem.getItemId();
+
+                if (id==R.id.adminview){
+                    Intent i = new Intent(getApplicationContext(), Mapped.class);
+                    startActivity(i);
+                }
+                return true;
+            }
+        });
+
+    }
+    @Override
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        return abdt.onOptionsItemSelected(item) || super.onOptionsItemSelected(item);
 
     }
 
