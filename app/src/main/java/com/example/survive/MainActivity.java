bf346dc6f86f7e756d8e3230d655b84137340aa3
naynikaw <nay9139@gmail.com>
2019-05-05 09:15:35 +0530

    Initial commit

---
+++ app/src/main/java/com/example/survive/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.survive;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.ebanx.swipebtn.OnStateChangeListener;
+import com.ebanx.swipebtn.SwipeButton;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    SwipeButton swipe_me;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        swipe_me = (SwipeButton) findViewById(R.id.swipe_me);
+        swipe_me.setOnStateChangeListener(new OnStateChangeListener() {
+            @Override
+            public void onStateChange(boolean active) {
+                Intent i = new Intent(getApplicationContext(), SaveMe.class);
+                startActivity(i);
+            }
+        });
+
+    }
+
+}
---
+++ app/src/main/java/com/example/survive/SaveMe.java
@@ -0,0 +1,97 @@
+package com.example.survive;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.media.MediaPlayer;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import android.app.Activity;
+import android.util.Log;
+
+import com.amazonaws.mobile.client.AWSMobileClient;
+import com.amazonaws.mobile.client.Callback;
+import com.amazonaws.mobile.client.UserStateDetails;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferUtility;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferState;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferObserver;
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferListener;
+import com.amazonaws.services.s3.AmazonS3Client;
+
+import java.io.File;
+
+import com.amazonaws.mobileconnectors.s3.transferutility.TransferService;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+
+public class SaveMe extends AppCompatActivity {
+
+    private static final String TAG = SaveMe.class.getSimpleName();
+
+    private FusedLocationProviderClient fusedLocationClient;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_save_me);
+
+        getApplicationContext().startService(new Intent(getApplicationContext(), TransferService.class));
+
+        final MediaPlayer alarm = MediaPlayer.create(this,R.raw.alarm_clock_samsung);
+
+        Button ringButton = (Button)this.findViewById(R.id.ringButton);
+        ringButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alarm.start();
+            }
+        });
+
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+
+        fusedLocationClient.getLastLocation()
+                .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                    @Override
+                    public void onSuccess(Location location) {
+                        if (location != null) {
+                            String la = String.valueOf(location.getLatitude());
+                            String lo = String.valueOf(location.getLongitude());
+                            TextView locationTextView = findViewById(R.id.locationTextView);
+
+                            locationTextView.setText(la + "\n" + lo);
+
+                        }
+                        else {
+                            TextView locationTextView = findViewById(R.id.locationTextView);
+                            locationTextView.setText("null");
+                        }
+                    }
+                });
+    }
+
+
+}
+
